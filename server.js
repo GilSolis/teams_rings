b52686a4f865c45cbb510dfaccce48768dde7f84
Gil Solis <gilsolisuw@gmail.com>
2021-03-30 19:53:58 -0700

    few styles

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.set('view engine', 'ejs')
-// app.use(express.static('public'))
-app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(express.static('public'))
+// app.use("/static", express.static(path.join(__dirname, "public")));
 
 
 app.get('/', (request, response) => {
